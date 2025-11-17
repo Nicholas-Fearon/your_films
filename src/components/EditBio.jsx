5500255a9343e8b599ecff17eddd39929598609e
Nicholas-Fearon <nejfearon@gmail.com>
2025-11-17 19:30:50 +0000

    edit profile updated

--- src/components/EditBio.jsx
+++ src/components/EditBio.jsx
@@ -1,25 +1,26 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/utils/db";
 
-export async function EditBio (){
-  const {userId} = await auth();
-  const responseUser= await db.query(
+export async function EditBio() {
+  const { userId } = await auth();
+  const responseUser = await db.query(
     `SELECT
     users.id,
     users.bio
-    FROM users`);
-    
-    async function handleEdit(formData) {
-      const bio = formData.get("bio")
-      await db.query(`UPDATE users SET bio =$1 WHERE clerk_id = $2, [bio, id]`);
-    }
+    FROM users`
+  );
 
-return(
-  <div>
-    <form action="handleEdit">
-   <textarea name="bio" className="text-black"></textarea>
-   <button>submit</button>
-    </form>     
-  </div>
-)
+  async function handleEdit(formData) {
+    const bio = formData.get("bio");
+    await db.query(`UPDATE users SET bio =$1 WHERE clerk_id = $2, [bio, id]`);
+  }
+
+  return (
+    <div>
+      <form action={handleEdit}>
+        <textarea name="bio" className="text-black"></textarea>
+        <button>submit</button>
+      </form>
+    </div>
+  );
 }
